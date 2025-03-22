1af59fed52bb1ba9d8b6b3005b97b598f4d9b177
Rulo77 <ragome1234567@gmail.com>
2025-03-22 17:23:12 -0600

    Auto-generated commit

---
+++ chunk-AUKFILXC.js
@@ -0,0 +1,1 @@
+import{Db as t,Eb as i,Wb as p,jb as o}from"./chunk-XLFHGM7U.js";var a=class n{static \u0275fac=function(e){return new(e||n)};static \u0275cmp=o({type:n,selectors:[["app-config"]],decls:2,vars:0,template:function(e,s){e&1&&(t(0,"p"),p(1,"config works!"),i())},encapsulation:2})};export{a as ConfigComponent};
